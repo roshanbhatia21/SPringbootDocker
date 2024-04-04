c542f47858f12a4c4cb4f30f10d37780ce1ec500
Roshan Bhatia <69297320+roshanbhatia21@users.noreply.github.com>
2024-04-04 15:21:50 +0530

    Update SimpleApplication.java

--- src/main/java/com/nagarro/SimpleApplication/SimpleApplication.java
+++ src/main/java/com/nagarro/SimpleApplication/SimpleApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SimpleApplication {
 
 	public static void main(String[] args) {
+		System.out.println("WELCOME");
 		SpringApplication.run(SimpleApplication.class, args);
 	}
 
